b70face494973caa8bc1adbf9070436cfa38b8db
Roman Koshla <koshla.roman10@gmail.com>
2020-03-06 18:54:16 +0400

    feat: add solution for morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let keys = Object.keys(MORSE_TABLE);
+    let value = Object.values(MORSE_TABLE);
+    let arrExpr = expr.split('')
+    let key = [];
+    let res = [];
+
+    for (let i = 0; i < arrExpr.length; ) {
+        if(arrExpr[i] == 1 && arrExpr[i + 1] == 0) {
+            key.push('.');
+        }
+        if(arrExpr[i] == 1 && arrExpr[i + 1] == 1) {
+            key.push('-');
+        }
+        if(arrExpr[i] == '*') {
+            res.push(' ');
+            i = i + 8;
+        }
+        if(i == 8){
+            let str = key.join('');
+            let index = keys.indexOf(str);
+            let letter = value[index];
+
+            res.push(letter);
+            key = [];
+            arrExpr.splice(0, 10);
+            i = -2;
+        }
+        i = i + 2;
+    }
+    return res.join('')
 }
 
 module.exports = {
